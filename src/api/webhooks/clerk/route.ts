b2d7009937d8b61704c18de3f3c81049605a6da8
abhispaceX <abhiramgaddam13@gmail.com>
2024-07-31 13:29:39 +0530

    changed

--- src/api/webhooks/clerk/route.ts
+++ src/api/webhooks/clerk/route.ts
@@ -63,7 +63,7 @@ export async function POST(req: Request) {
   // CREATE
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
-     console.log(id,email_addresses)
+    
 
     const user = {
       clerkId: id,
